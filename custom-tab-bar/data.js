21133065ff9ff1055b9544339d6ff7d7cfb9df22
蔡武 <2252711582@qq.com>
2023-06-06 00:59:44 +0800

    init

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    icon: 'home',
+    text: '首页',
+    url: 'pages/home/home',
+  },
+  {
+    icon: 'remind',
+    text: '其他',
+    url: 'pages/other/other',
+  },
+  {
+    icon: 'person',
+    text: '个人中心',
+    url: 'pages/usercenter/index',
+  },
+];
